43b06ebd9f34878336836ceefcca3ba375eb997b
Tomaso2468 <48136416+Tomaso2468@users.noreply.github.com>
2019-05-12 11:32:54 +0100

    Incremented version number to 0.6.1

--- RPGOnlineEngine/src/rpgonline/RPGOnline.java
+++ RPGOnlineEngine/src/rpgonline/RPGOnline.java
@@ -28,13 +28,13 @@ public final class RPGOnline {
 			new Version("0.3.4+1550318158"), new Version("0.3.5+1551121021"), new Version("0.3.6+1551364566"),
 			new Version("0.3.7+1551629276"), new Version("0.3.8+1551632798"), new Version("0.3.9+1551724618"),
 			new Version("0.3.10+1552726489"), new Version("0.3.11+1554057287"), new Version("0.3.12+1554479444"),
-			new Version("0.4.0+1555165066"), new Version("0.5.0+1556906194"), new Version("0.6.0+1557583232") };
+			new Version("0.4.0+1555165066"), new Version("0.5.0+1556906194"), new Version("0.6.0+1557583232"), new Version("0.6.1+1557657120") };
 	/**
 	 * <p>
 	 * The current version of the RPGOnline library.
 	 * </p>
 	 */
-	public static final Version VERSION = new Version("0.6.0+1557583232");
+	public static final Version VERSION = new Version("0.6.1+1557657120");
 	/**
 	 * <p>
 	 * The version of java that this library was compiled with.
